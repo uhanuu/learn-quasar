b10b9c86f85c08619079729e12e13f9b16738f03
uhanuu <hwryu990927@gmail.com>
2024-07-18 12:11:30 +0900

    feat: instagram profile

--- src/pages/FlexGrid2.vue
+++ src/pages/FlexGrid2.vue
@@ -1,12 +1,77 @@
 <template>
-  <q-page class="q-pa-xl">
-    <section class="q-mb-xl">
-      <div class="text-h4">FlexGrid2</div>
-      <q-separator class="q-my-md" />
+  <q-page padding>
+    <section class="row q-col-gutter-x-xl q-py-lg">
+      <div class="col-12 col-sm-auto q-py-md flex flex-center">
+        <q-avatar size="150px">
+          <q-img src="/this-logo.png" />
+        </q-avatar>
+      </div>
+      <div class="col-12 col-sm-grow column justify-between">
+        <div class="row items-center">
+          <span class="text-h6 text-weight-bold">uhanuu</span>
+          <q-space />
+          <div class="q-gutter-x-sm">
+            <q-btn unelevated class="profile_btn" label="프로필 편집" />
+            <q-btn unelevated class="profile_btn" label="보관된 스토리 보기" />
+            <q-btn unelevated class="q-pa-xs" flat icon="settings" />
+          </div>
+        </div>
+        <div class="q-gutter-x-lg">
+          <div class="inline-block">
+            게시물 <span class="text-weight-bolder">6</span>
+          </div>
+          <div class="inline-block">
+            팔로워 <span class="text-weight-bolder">368</span>
+          </div>
+          <div class="inline-block">
+            팔로우 <span class="text-weight-bolder">349</span>
+          </div>
+        </div>
+        <div class="column">
+          <span>유현우</span>
+          <span>@u_olyx</span>
+        </div>
+      </div>
+    </section>
+    <section>
+      <div class="row no-wrap scroll q-col-gutter-x-xl">
+        <div class="col-auto" v-for="n in 15" :key="n">
+          <article>
+            <q-avatar size="70px">
+              <q-img src="/this-logo.png" />
+            </q-avatar>
+            <div class="text-center q-mt-sm">제목</div>
+          </article>
+        </div>
+      </div>
+    </section>
+    <section class="q-mt-xl">
+      <div class="row q-col-gutter-md">
+        <div v-for="n in 18" :key="n" class="col-12 col-sm-6 col-md-4 col-lg-3">
+          <q-card class="my-card">
+            <q-img ratio="1" src="https://cdn.quasar.dev/img/parallax2.jpg">
+              <div
+                class="absolute-bottom text-subtitle2 flex flex-center full-height"
+              >
+                Title
+              </div>
+            </q-img>
+          </q-card>
+        </div>
+      </div>
     </section>
   </q-page>
 </template>
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.q-page {
+  background-color: #fafafa;
+}
+.profile_btn {
+  background-color: #efefef;
+  font-weight: 800;
+  border-radius: 8px;
+}
+</style>
